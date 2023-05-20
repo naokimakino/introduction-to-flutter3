47a5ebe69078ff506c4e57cc639b426755485cd3
Naoki Makino <n.makino.0930@gmail.com>
2023-05-20 12:43:22 +0900

    feat(chapter01): list2_2

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Text(
-        'Hello, Flutter World!!',
-        style: TextStyle(fontSize: 32.0),
-      ),
-    );
+        title: 'Flutter Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Hello Flutter!'),
+          ),
+          body: Text(
+            'Hello, Flutter World!!',
+            style: TextStyle(fontSize: 32.0),
+          ),
+        ));
   }
 }
