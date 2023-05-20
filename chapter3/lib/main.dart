7b0ed626f80b0f494112ac696966af622ac768a4
Naoki Makino <n.makino.0930@gmail.com>
2023-05-20 19:08:13 +0900

    feat(chapter3): list3_17

--- chapter3/lib/main.dart
+++ chapter3/lib/main.dart
@@ -28,7 +28,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static const _message = 'ok.';
+  static var _message = 'ok.';
 
   @override
   Widget build(BuildContext context) {
@@ -42,11 +42,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.all(20.0),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
                 child: Text(
                   _message,
-                  style: TextStyle(
+                  style: const TextStyle(
                       fontSize: 32.0,
                       fontWeight: FontWeight.w400,
                       fontFamily: "Roboto"),
@@ -76,9 +76,25 @@ class _MyHomePageState extends State<MyHomePage> {
   void buttonPressed() {
     showDialog(
         context: context,
-        builder: (BuildContext context) => const AlertDialog(
+        builder: (BuildContext context) => AlertDialog(
               title: Text("Hello!"),
               content: Text("This is sample."),
-            ));
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () => Navigator.pop<String>(context, 'Cancel'),
+                ),
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () => Navigator.pop<String>(context, 'OK'),
+                )
+              ],
+            )).then<void>((value) => resultAlert(value));
+  }
+
+  void resultAlert(String value) {
+    setState(() {
+      _message = 'selected: $value';
+    });
   }
 }
