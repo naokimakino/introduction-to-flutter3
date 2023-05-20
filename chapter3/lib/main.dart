bd0475821b2ec1915c45ba1c7417488ea38a5349
Naoki Makino <n.makino.0930@gmail.com>
2023-05-20 21:35:05 +0900

    feat(chapter3): list3_18

--- chapter3/lib/main.dart
+++ chapter3/lib/main.dart
@@ -76,18 +76,21 @@ class _MyHomePageState extends State<MyHomePage> {
   void buttonPressed() {
     showDialog(
         context: context,
-        builder: (BuildContext context) => AlertDialog(
-              title: Text("Hello!"),
-              content: Text("This is sample."),
-              actions: <Widget>[
-                TextButton(
-                  child: Text('Cancel'),
-                  onPressed: () => Navigator.pop<String>(context, 'Cancel'),
+        builder: (BuildContext context) => SimpleDialog(
+              title: const Text("Select assignment"),
+              children: [
+                SimpleDialogOption(
+                  onPressed: () => Navigator.pop<String>(context, "One"),
+                  child: const Text("One"),
                 ),
-                TextButton(
-                  child: Text('OK'),
-                  onPressed: () => Navigator.pop<String>(context, 'OK'),
-                )
+                SimpleDialogOption(
+                  onPressed: () => Navigator.pop<String>(context, "Two"),
+                  child: const Text("Two"),
+                ),
+                SimpleDialogOption(
+                  onPressed: () => Navigator.pop<String>(context, "Three"),
+                  child: const Text("Three"),
+                ),
               ],
             )).then<void>((value) => resultAlert(value));
   }
