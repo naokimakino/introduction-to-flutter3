0e4daa48371be2e663f19f4bae8ee6e6648096a4
Naoki Makino <n.makino.0930@gmail.com>
2023-05-20 22:19:52 +0900

    feat(chapter4): list4_4

--- chapter4/lib/main.dart
+++ chapter4/lib/main.dart
@@ -28,7 +28,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static const _message = 'ok.';
+  static var _message = 'ok.';
+  static var _index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -38,30 +39,60 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: <Widget>[
-          const Text(
+          Text(
             _message,
-            style: TextStyle(fontSize: 32.0),
+            style: const TextStyle(fontSize: 32.0),
           ),
           ListView(
             shrinkWrap: true,
             padding: const EdgeInsets.all(20.0),
-            children: const [
-              Text(
-                'First item',
-                style: TextStyle(fontSize: 24.0),
+            children: [
+              ListTile(
+                leading: const Icon(Icons.android, size: 32),
+                title: const Text(
+                  'first item',
+                  style: TextStyle(fontSize: 28),
+                ),
+                selected: _index == 1,
+                onTap: () {
+                  _index = 1;
+                  tapTile();
+                },
               ),
-              Text(
-                'Second item',
-                style: TextStyle(fontSize: 24.0),
+              ListTile(
+                leading: const Icon(Icons.android, size: 32),
+                title: const Text(
+                  'second item',
+                  style: TextStyle(fontSize: 28),
+                ),
+                selected: _index == 2,
+                onTap: () {
+                  _index = 2;
+                  tapTile();
+                },
               ),
-              Text(
-                'Third item',
-                style: TextStyle(fontSize: 24.0),
+              ListTile(
+                leading: const Icon(Icons.android, size: 32),
+                title: const Text(
+                  'third item',
+                  style: TextStyle(fontSize: 28),
+                ),
+                selected: _index == 3,
+                onTap: () {
+                  _index = 3;
+                  tapTile();
+                },
               ),
             ],
           )
         ],
       ),
     );
+  }
+
+  void tapTile() {
+    setState(() {
+      _message = 'you tapped: No, $_index.';
+    });
   }
 }
