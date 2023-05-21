72f724363874fb2a21bf8a7cd8e17779b14b0445
Naoki Makino <n.makino.0930@gmail.com>
2023-05-21 10:47:31 +0900

    feat(chapter4): list4_7

--- chapter4/lib/main.dart
+++ chapter4/lib/main.dart
@@ -21,19 +21,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  static final _controller = TextEditingController();
+  static var _input = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: const Center(
-        child: Text(
-          'Home Screen',
+      body: Column(children: <Widget>[
+        const Text(
+          'HomeScreen',
           style: TextStyle(fontSize: 32.0),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            controller: _controller,
+            style: const TextStyle(fontSize: 28.0),
+            onChanged: changeField,
+          ),
+        )
+      ]),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 1,
         items: const <BottomNavigationBarItem>[
@@ -45,24 +61,28 @@ class FirstScreen extends StatelessWidget {
         onTap: (int value) => {
           if (value == 1)
             Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const SecondScreen()))
+                MaterialPageRoute(builder: (context) => SecondScreen(_input)))
         },
       ),
     );
   }
+
+  void changeField(String val) => _input = val;
 }
 
 class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
+  final String _value;
+
+  const SecondScreen(this._value, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Next')),
-      body: const Center(
+      body: Center(
         child: Text(
-          'Next Screen',
-          style: TextStyle(fontSize: 32.0),
+          'you typed: "$_value".',
+          style: const TextStyle(fontSize: 32.0),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
